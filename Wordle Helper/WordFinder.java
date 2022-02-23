f29832a196f1b513ceb11afe07115e24a7ac2967
Anurag <anurag.tilwe6@gmail.com>
2022-02-23 12:11:10 -0500

    Wordle Helper
    Added letters not in word feature to WordFinder

--- Wordle Helper/WordFinder.java
+++ Wordle Helper/WordFinder.java
@@ -26,13 +26,14 @@ public class WordFinder
 		System.out.println("INSTRUCTIONS\nEnter a capital letter for a letter in the correct spot\nEnter a lowercase letter for a letter in the word but not in that position\nEnter a \"-\" for spot with no information\nEnter \"x\" to exit");
 		while (true)
 		{
-			goodWords.clear();
 			System.out.print("\n"+l+" letter word: ");
 			String s = in.nextLine();
+			System.out.print("Enter letters that are not in the word (if none just press enter): ");
+			ArrayList<String> badLetters = new ArrayList<String>(Arrays.asList(in.nextLine().split("")));	//Initializes badLetters ArrayList with input
 			if (s.equalsIgnoreCase("x")) break;	//Ends when user enters "x"
 			while (s.length()!=l && !s.equalsIgnoreCase("x"))	//Makes sure input is a valid 5 letter entry
 			{
-				System.out.print("\nEntry invalid. Enter "+l+" letter word: ");
+				System.out.print("\nEntry word invalid. Enter "+l+" letter word: ");
 				s = in.nextLine();
 			}
 			if (s.equalsIgnoreCase("x")) break;	//Ends when user enters "x"
@@ -55,20 +56,27 @@ public class WordFinder
 			}
 
 			//Fills in goodWords ArrayList with words that satisfy the given conditions
-			boolean add = false;
 			for (int i=0; i<words.size(); i++)
 			{
 				String w = words.get(i);
 				int correctCountCheck = correctCount, almostCountCheck = almostCount;
+				boolean add = true;
 				for (int j=0; j<l; j++)
 				{
+					for (int k=0; k<badLetters.size(); k++)
+						if (w.contains(badLetters.get(k)))
+						{
+							add = false;
+							break;
+						}
+					if (!add) break;
 					if ((w.charAt(j)+"").equalsIgnoreCase(correct[j]))
 						correctCountCheck--;
 					else if (almost[j] != null)
 						if (w.contains(almost[j]))
 							almostCountCheck--;
 				}
-				if (correctCountCheck==0 && almostCountCheck==0)
+				if (correctCountCheck==0 && almostCountCheck==0 && add)
 					goodWords.add(w);
 			}
 
@@ -78,6 +86,9 @@ public class WordFinder
 			System.out.println("\nPotential answers for the wordle:");
 			for (String gw : goodWords)
 				System.out.println(gw);
+
+			goodWords.clear();
+			badLetters.clear();
 		}
 	}//WordFinder
 
